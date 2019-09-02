af0e5097c1a64068ee8213cbca6dd041c5c67c72
KKUNDO <kenan.kundo@otto.de>
2019-09-02 13:58:05 +0200

    added datetimefct

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Main {
     public static void main (String[] args) {
-        System.out.println("HEYA WORLDS, IS ME DE GREAT AUGUSTOSINIO");
+        //System.out.println("HEYA WORLDS, IS ME DE GREAT AUGUSTOSINIO");
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        String time = dateTimeFormatter.format(LocalDateTime.now());
+        System.out.println(time); //Seperation-symbol is ":"
+
     }
 }
